89d08238e43b2adda600016a718b41e72fb387f0
Chris McIntosh <christopher.l.mcintosh@outlook.com>
2020-04-16 14:33:22 -0500

    Login works
    Also added a before_action in ApplicationController to
    `:authenticate_user!`

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'users/sessions#new'
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new"
+    get "sign_out", to: "devise/sessions#destroy"
+  end
+
+  root to: 'users#home'
 end
