a4347700a68d22198f91fa5a7c0fc96ea0fe8554
kreestoff <christopher.l.mcintosh@outlook.com>
2020-04-21 10:08:23 -0500

    Revert "Created some base models needed to create an MVP"

--- db/migrate/20200420192323_create_grocery_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateGroceryLists < ActiveRecord::Migration[6.0]
-  def change
-    create_table :grocery_lists do |t|
-      t.string :title, limit: 20
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200420192333_create_recipes.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecipes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :recipes do |t|
-      t.string :title, limit: 20
-
-      t.timestamps
-    end
-  end
-end
